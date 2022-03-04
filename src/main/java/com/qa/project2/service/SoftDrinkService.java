e800c372314d2b3a159e85f54a00d2b854b3a409
adilakbarali <adilakbarali@gmail.com>
2022-03-04 11:39:05 +0000

    Added service and controller classes

---
+++ src/main/java/com/qa/project2/service/SoftDrinkService.java
@@ -0,0 +1,49 @@
+package com.qa.project2.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.qa.project2.domain.SoftDrink;
+import com.qa.project2.repo.SoftDrinkRepo;
+
+@Service
+public class SoftDrinkService implements ServiceInterface<SoftDrink>{
+
+	private SoftDrinkRepo repo;
+	
+	@Autowired
+	public SoftDrinkService(SoftDrinkRepo repo) {
+		this.repo = repo;
+	}
+	
+	public SoftDrink create(SoftDrink s) {
+		SoftDrink created = this.repo.save(s);
+		return created;
+	}
+
+	public List<SoftDrink> getAll() {
+		return this.repo.findAll();
+	}
+
+	public SoftDrink get(Integer id) {
+		Optional<SoftDrink> found = this.repo.findById(id);
+		return found.get();
+	}
+
+	public SoftDrink replace(Integer id, SoftDrink s) {
+		SoftDrink existing = this.repo.findById(id).get();
+		existing.setBrand(s.getBrand());
+		existing.setFlavour(s.getFlavour());
+		existing.setSizeLitres(s.getSizeLitres());
+		SoftDrink updated = this.repo.save(existing);
+		return updated;
+	}
+
+	public void remove(Integer id) {
+		this.repo.deleteById(id);
+	}
+
+}
