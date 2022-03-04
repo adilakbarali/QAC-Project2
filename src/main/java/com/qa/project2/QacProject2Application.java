8327a79a10ec1e2b6f5aa3f3f6a72239edacf3c7
adilakbarali <adilakbarali@gmail.com>
2022-03-04 09:04:32 +0000

    Initial Commit

---
+++ src/main/java/com/qa/project2/QacProject2Application.java
@@ -0,0 +1,13 @@
+package com.qa.project2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QacProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QacProject2Application.class, args);
+	}
+
+}
